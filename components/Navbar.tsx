df3f3e78c5a9b869411b3bb0985b63e932e2c557
Michael Carr <xyeres@me.com>
2021-10-06 16:48:12 -0400

    Add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+import Link from 'next/link';
+
+// top navbar
+export default function Navbar() {
+  const { user, username } = useContext(UserContext);
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">// DEV.feed</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
