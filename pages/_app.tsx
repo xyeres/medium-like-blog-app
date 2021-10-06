d4b60f76af63fa15225198cdcc7dcc7b32b341d9
Michael Carr <xyeres@me.com>
2021-10-06 16:48:44 -0400

    Add userData hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
+
 
 // Wrapper for app, use this to add global components 
 // like Nav bar or auth state 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
