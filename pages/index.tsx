365210747ff9ad8baeb8f22ba2ccd923397e6c94
Michael Carr <xyeres@me.com>
2021-10-06 16:49:07 -0400

    Add toast test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
+
 
 export default function Home() {
   return (
     <div>
-
+      <button onClick={() => toast.success('hello from toaster!')}>Toast me!</button>
     </div>
   )
 }
