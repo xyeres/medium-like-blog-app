3619c4f4443f3eddf1f603f86645e3dbe1c93774
Michael Carr <xyeres@me.com>
2021-10-08 17:01:32 -0400

    Add post feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,64 @@ import styles from '../styles/Home.module.css'
 import Loader from '../components/Loader';
 import toast from 'react-hot-toast';
 
+import { firestore, postToJSON, fromMillis } from '../lib/firebase';
+import { useState } from 'react';
+import PostFeed from '../components/PostFeed';
 
-export default function Home() {
+// max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps() {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT)
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  }
+}
+
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false)
+  const [postsEnd, setPostsEnd] = useState(false)
+
+  const getMostPosts = async () => {
+    setLoading(true)
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT)
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data())
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false)
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true)
+    }
+  }
   return (
-    <div>
-      <button onClick={() => toast.success('hello from toaster!')}>Toast me!</button>
-    </div>
+    <main>
+      <PostFeed admin={false} posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMostPosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
   )
 }
